c6c5423200b7f2f51c273471b6d5a6a90f007b8a
sai sumanth reddy gurrala <saisumanthreddy76@gmail.com>
2024-04-16 17:29:25 -0400

    Initialization of the server using spring initializer
    1. Initialized the project using spring initializer and added the necessary depencies.
    2. This project runs on Java 17, Spring boot, and mySQL as database and maven as build tool.

---
+++ Server/src/test/java/com/recipemanagementsystem/demo/RecipeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipemanagementsystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
