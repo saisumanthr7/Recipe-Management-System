969da3483e8c15a60d0b38a89863a81ac7ad100c
sai sumanth reddy gurrala <saisumanthreddy76@gmail.com>
2024-05-02 10:37:44 -0400

    Reorganised the application
    1. Used ng new --no-standalone to create the application for getting the necessary routing and module files.
    2. Reorganized the application.

--- Client/Recipe_Management_System/src/app/app.component.spec.ts
+++ Client/Recipe_Management_System/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-bootstrap' title`, () => {
+  it(`should have as title 'Recipe_Management_System'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-bootstrap');
+    expect(app.title).toEqual('Recipe_Management_System');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-bootstrap');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Recipe_Management_System');
   });
 });
